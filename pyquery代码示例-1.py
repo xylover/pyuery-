10855ace98653a991e8ee6b28d2b06da50b7148c
xylover <63713441+xylover@users.noreply.github.com>
2020-07-03 17:18:47 +0800

    first commit

---
+++ pyquery代码示例-1.py
@@ -0,0 +1,15 @@
+html = '''
+<div>
+<ul>
+<li class="item-0"><a href="link1.html">first item</a></li>
+<li class="item-1"><a href="link2.html">second item</a></li>
+<li class="item-inactive"><a href="link3.html">third item</a></li>
+<li class="item-1"><a href="link4.html">fourth item</a></li>
+<li class="item-0"><a href="link5.html">fifth item</a>
+</ul>
+</div>
+'''
+
+from pyquery import PyQuery as pq
+doc = pq(html)
+print(doc('li'))
